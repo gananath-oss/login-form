455ffb49d16e6c8f08da4a959c9034402391d26d
gananath-oss <ganaya2k17@gmail.com>
2021-03-17 01:46:14 +0530

    add-the-js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+
+function register() {
+	x.style.left = "-1630px";
+	y.style.left = "130px";
+	z.style.left = "500px";
+}
+
+function login() {
+	x.style.left = "130px";
+	y.style.left = "1630px";
+	z.style.left = "0";
+}
